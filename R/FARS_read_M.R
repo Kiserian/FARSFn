385875e6dcf3f0b33449a2b138f8aab2c71d5619
Kiserian <daggy_862@hotmail.com>
2017-05-25 20:59:09 -0400

    First load

---
+++ R/FARS_read_M.R
@@ -0,0 +1,38 @@
+#'Read data from multiple years
+#'
+#'This function reads the FARS data of multiple years, and proceses the
+#'number of incidents per month and year.
+#'
+#'@param years A numeric vector or a list containing the years of the
+#'data desired.
+#'
+#'@details This function uses the elements on the vector or list \code{years}
+#'to create filenames through \link{make_filename()} and read each file with
+#'\link{fars_read()}. Then selects the month and year columns of the data.
+#'A mechanism to detect invalid years is implemented to display
+#'warnings when invalid values are used as input.
+#'
+#'@return The function returns a list of data frames containing the loaded data.
+#'In case invalid outputs are used, the returned value is NULL, prior printing a
+#'warning on the console.
+#'
+#'@examples
+#'\dontrun{a <- c(1998, 2001, 2005)
+#'data <- fars_read_years(a)}
+#'\dontrun{ a <- list(1998, 2001, 2005)
+#'data <- fars_read_years(a)}
+#'
+#'@export
+fars_read_years <- function(years) {
+        lapply(years, function(year) {
+                file <- make_filename(year)
+                tryCatch({
+                        dat <- fars_read(file)
+                        dplyr::mutate(dat, year = year) %>%
+                                dplyr::select(MONTH, year)
+                }, error = function(e) {
+                        warning("invalid year: ", year)
+                        return(NULL)
+                })
+        })
+}
